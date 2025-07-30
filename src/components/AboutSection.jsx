c281e85a8c9af2eb09fb78f7f1035fba65fc433e
AdvaithRakshan <143710607+AdvaithRakshan@users.noreply.github.com>
2025-07-30 17:27:05 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public/Advaith Resume 7.pdf" download
+                href="public/AdvaithResume7.pdf" download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
